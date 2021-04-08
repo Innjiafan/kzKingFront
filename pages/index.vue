a7e6c56e02457757d4c046706b3f1e2cc828b5db
jiafan <1197061567@qq.com>
2021-04-08 22:44:40 +0800

    快转王首页

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <section class="banner">
+      <div class="w1200">
+        <section class="nav my-flex">
+          <div class="logo-box my-flex">
+            <img src="@/assets/images/icon/icon-logo.svg" alt="">
+            <div class="r">
+              <h4>快转王办公助手</h4>
+              <p>高效易用  在线办公必备</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .w1200{
+    width: 1260px;
+    margin: 0 auto;
+  }
+  .banner{
+    width:100%;
+    height: 70px;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+  .nav{
+    display: flex;
+    align-items: center;
+    height: 70px;
+    .logo-box{
+      img{
+        width: 42px;
+        height: 42px;
+        margin-right: 10px;
+      }
+      h4{
+        font-size: 18px;
+        font-weight: bold;
+        color: #1A1A1A;
+        line-height: 18px;
+        margin-bottom: 2px;
+      }
+      p{
+        color: #BBBBBB;
+      }
+    }
+  }
+</style>
